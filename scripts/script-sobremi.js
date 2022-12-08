a7b98d2f020283334cc98602c2283175b8bcb9a7
Brandon Cuéllar <branncg2@gmail.com>
2022-12-08 15:05:02 -0400

    Fotografia 1.0

---
+++ scripts/script-sobremi.js
@@ -0,0 +1,13 @@
+var img1='img/k1.png';
+var img2='img/k2.png';
+var booleano=true;
+setInterval(() => {
+    if (booleano) {
+        document.getElementById("fotok").style.backgroundImage = "url("+img2+")"
+        booleano=false;    
+    } else {
+        document.getElementById("fotok").style.backgroundImage = "url("+img1+")"
+        booleano=true;  
+    }
+        
+}, 5000);
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let var1=document.querySelectorAll(".modal img");
+console.log(var1);
+var1.forEach(el=>{
+    el.addEventListener("click",function(ev){
+        ev.stopPropagation();
+        this.parentNode.classList.add("active");
+        this.parentNode.querySelector(".difuminado").style.display='none';
+            
+    })
+});
+
+document.querySelectorAll(".modal").forEach(el=>{
+    el.addEventListener("click",function(ev){
+        this.classList.remove("active");
+        this.parentNode.querySelector(".difuminado").style.display='block';     
+    })
+});
